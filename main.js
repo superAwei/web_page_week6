eb8f7593f793c0b14e47acd3b26a121a462c3ebb
AWEI <challengewei0625@gmail.com>
2023-09-05 10:08:05 +0800

    詢問問題

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
 console.log("Hello world!");
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    speed: 20000,
+    loop: true,
+    autoplay: {
+      delay: 1
+    }
+  });
